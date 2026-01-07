f8c779f119995ac5c620a58c600a8fa143e9b7fa
Aryan Chaudhary <j.aryanchaudhary@gmail.com>
2026-01-08 01:54:46 +0530

    fix: correct API base URL env variable for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // http://localhost:3000
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
+
 
 export default axiosInstance;
